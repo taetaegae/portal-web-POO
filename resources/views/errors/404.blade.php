44e895bee558bd4dd2aa23498e05b21db11ba410
Andre <a77870066@gmail.com>
2026-05-31 22:49:34 -0600

    Mejore la pagina de error y ademas le puse style

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,13 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Error 404</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    @vite(['resources/css/app.css'])
 </head>
 <body>
-    <h1>Error 404</h1>
-    <p>La página que estás buscando no fue encontrada.</p>
 
-    <a href="inicio.blade.php">Volver al inicio</a>
+<div class="error-page">
+
+    <h1>404</h1>
+
+    <h2 class="mb-3">
+        Página no encontrada
+    </h2>
+
+    <p class="mb-4">
+        Lo sentimos, la página que intentas visitar no existe o fue movida.
+    </p>
+
+    <a href="/" class="btn btn-dark btn-lg">
+        Volver al Inicio
+    </a>
+
+</div>
 
 </body>
 </html>
